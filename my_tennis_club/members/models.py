83298ad432b972018eb6b9880a2cd80e6a9aa62a
Daniel7 <dl4041569@gmail.com>
2025-11-20 11:00:56 -0500

    Correciones y mejoras al proyecto: Agreacion de css para los botonoes y mejoras en la muestra de informacion

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,4 +1,7 @@
+from enum import member
 from django.db import models
+
+
 
 # Create your models here.
 class Member(models.Model):
@@ -8,3 +11,6 @@ class Member(models.Model):
     joined_date = models.DateField(null=True)
     email = models.EmailField(max_length=254, null=True)
     address = models.CharField(max_length=255, null=True)
+    
+def __str__(self):
+    return f"{self.firstname} {self.lastname}"
