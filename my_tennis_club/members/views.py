7550914a98d3f54f7a6e49b44be66d06054cee3d
Daniel7 <dl4041569@gmail.com>
2025-11-25 06:47:40 -0500

    Actividad 24 11 25: Agregar atributos en la funcion testing y pruebas de comandos en base de datos

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Member
+from django.db.models import Q
 
 def members(request):
   mymembers = Member.objects.all().values()
@@ -36,8 +37,34 @@ def main(request):
 def testing(request):
   template = loader.get_template('template.html')
   miembros = Member.objects.all().values()
+  column_fistname = miembros.values_list('first_name')
+  records_daniel = Member.objects.filter(firstname='Daniel')
+  record_AND_daniel = Member.objects.filter(first_name='Daniel', id=11).values()
+  record_OR_daniel = Member.objects.filter(Q(first_name='Violeta') | Q(first_name='Orlando')).values()
+  record_like_start_M = Member.objects.filter(first_name__startswith='M').values()
+  record_like_ends_S = Member.objects.filter(first_name__endswith='s').values()
+  record_like_contains_ez = Member.objects.filter(first_name__contains='ez').values()
+  record_like_icontains_z = Member.objects.filter(first_name__icontains='z').values()
+  record_range_id = Member.objects.filter(id__range=(2, 5)).values()
+  order_by_asc = Member.objects.all().order_by('first_name').values()
+  order_by_desc = Member.objects.all().order_by('-first_name').values()
+  
+  
   context = {
     'fruits': ['Apple', 'Banana', 'Cherry'],
-    'miembros' : miembros,   
-  }
+    'miembros' : miembros,
+    'column_fistname': column_fistname,
+    'records_daniel': records_daniel,
+    'record_AND_daniel': record_AND_daniel,
+    'record_OR_daniel': record_OR_daniel,
+    'record_like_start_M': record_like_start_M,
+    'record_like_ends_S': record_like_ends_S,
+    'record_like_contains_ez': record_like_contains_ez,
+    'record_like_icontains_z': record_like_icontains_z,
+    'record_range_id': record_range_id,
+    'order_by_asc': order_by_asc,
+    'order_by_desc': order_by_desc,
+  }   
+  
   return HttpResponse(template.render(context, request))
+  
